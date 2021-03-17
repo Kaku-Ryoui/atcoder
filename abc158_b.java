945e8eba884fa652d1877e8be6b9bdd0c5f5892c
kakun <mlis_1112@yahoo.co.jp>
2021-03-17 19:42:45 +0900

    auto commit

---
+++ abc158_b.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+/* 問題文javac -encoding utf-8 Main.java  */
+
+class abc158_b{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("sample");
+  }
+}
