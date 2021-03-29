2205372b518a6e3e4202ab425987a867bee201bb
kakun <mlis_1112@yahoo.co.jp>
2021-03-29 20:14:03 +0900

    auto commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+/* 問題文
+今、日本は 11 月 18 日ですが、11 と 18 は隣り合うリュカ数です。
+整数 N が与えられるので、N 番目のリュカ数を求めてください。
+ただし、リュカ数は i 番目のリュカ数を Li とすると、
+・L0=2
+・L1=1
+・Li=Li−1+Li−2(i≧2)
+と定義される数とします。
+制約
+・1≦N≦86
+・答えは 1018 より小さいことが保証される
+・入力は整数からなる
+javac -encoding utf-8 Main.java  */
+
+class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("sample");
+  }
+}
