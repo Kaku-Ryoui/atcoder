79dc09cdc8abb95f93edeee857cb57144e3f2630
kakun <mlis_1112@yahoo.co.jp>
2021-04-01 20:24:21 +0900

    auto commit

--- Main.java
+++ Main.java
@@ -1,23 +1,27 @@
 import java.util.*;
+import java.math.BigInteger;
 
 /* 問題文
-今、日本は 11 月 18 日ですが、11 と 18 は隣り合うリュカ数です。
-整数 N が与えられるので、N 番目のリュカ数を求めてください。
-ただし、リュカ数は i 番目のリュカ数を Li とすると、
-・L0=2
-・L1=1
-・Li=Li−1+Li−2(i≧2)
-と定義される数とします。
+問題文
+2 つの正整数 A,B が与えられるので、その大小を比較してください。
 制約
-・1≦N≦86
-・答えは 1018 より小さいことが保証される
-・入力は整数からなる
+・1≦A,B≦10＾100
+・入力の A,B の先頭は0でない。
 javac -encoding utf-8 Main.java  */
 
+// class Main{
+//   public static void main(String[] args){
+//     Scanner sc = new Scanner(System.in);
+//     BigInteger A = sc.nextBigInteger();
+//     BigInteger B = sc.nextBigInteger();
+//     System.out.println(A.compareTo(B)> 0 ? "GREATER" : A.compareTo(B) == 0 ? "EQUAL" : "LESS");
+//   }
+// }
 class Main{
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
-
-    System.out.println("sample");
+    double A=sc.nextDouble();
+		double B=sc.nextDouble();
+    System.out.println(A > B ? "GREATER" : A == B ? "EQUAL" : "LESS");
   }
 }
